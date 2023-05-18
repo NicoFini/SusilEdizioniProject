9b6245ab6b7a070904d216e12211c3f496323a79
Nico Fini <nicofini91@gmail.com>
2023-05-18 17:39:45 +0200

    General Fix

--- src/Comments.Core/Model/Book.cs
+++ src/Comments.Core/Model/Book.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SunsilEdizioni.Core.Model
+﻿namespace SunsilEdizioni.Core.Model
 {
     public class Book
     {
